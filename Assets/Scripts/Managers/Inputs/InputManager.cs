052048a8e77f5a02143d5b1be27fcd8a2cc3680e
Gabriel <55121175+Vannerss@users.noreply.github.com>
2023-10-28 08:05:26 -0500

    Added a basic character movement and input manager

---
+++ Assets/Scripts/Managers/Inputs/InputManager.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Managers.Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        public static InputManager Instance;
+        
+        private InputController _inputController;
+        private InputAction _move;
+        private InputAction _interact;
+
+        public event Action<bool> OnInteract;
+        
+        
+        private void Awake()
+        {
+            _inputController = new InputController();
+            
+            //Created a singleton of the object.
+            if (Instance != null)
+                Destroy(this.gameObject);
+
+            Instance = this;
+        }
+
+        private void OnEnable()
+        {
+            _move = _inputController.Player.Move;
+            _move.Enable();
+
+            _interact = _inputController.Player.Interact;
+            _interact.started += Interact;
+            _interact.canceled += Interact;
+            _interact.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _move.Disable();
+            
+            _interact.started -= Interact;
+            _interact.canceled -= Interact;
+            _interact.Disable();
+        }
+
+        public Vector2 GetMovementDirection()
+        {
+            return _move.ReadValue<Vector2>();
+        }
+
+        private void Interact(InputAction.CallbackContext context)
+        {
+            //if interact input was pressed, isInteracting = true, if input was released isInteract = false.
+            bool isInteracting = context.started;
+
+            OnInteract?.Invoke(isInteracting);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Managers.Inputs;
+
+namespace Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5f;
+        
+        private InputManager _input;
+        private CharacterController _controller;
+        
+        private void Start()
+        {
+            _input = InputManager.Instance;
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 inputDirection = _input.GetMovementDirection();
+            
+            Vector3 moveDirection = new Vector3(inputDirection.x, inputDirection.y, 0f).normalized;
+            _controller.Move(moveDirection * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
